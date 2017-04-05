976bc60753315f11f56b473b364a8f07add68703
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-04-05 23:19:10 +0200

    Support Android devices, pre-seed apps, license and iOS Developer Disks

--- install.sh
+++ install.sh
@@ -10,25 +10,49 @@ echo "ubuntu:ubuntu" | chpasswd
 
 # Install python for simple scripting
 apt-get install -y python python-pip
-pip install quamotion
+pip install selenium quamotion
 
-# usbmuxd enables connections to iOS devices
+# usbmuxd enables connections to iOS devices, and android-sdk-platform-tools-common adds
+# udev rules for Android devices
 apt-get install -y usbmuxd
+apt-get install -y android-sdk-platform-tools-common
 apt-get install -y jq libimobiledevice-utils
 
 # Download and install the Quamotion software
-wget -nv -nc https://qmcdn.blob.core.windows.net/download/quamotion-webdriver.0.1.6515-ubuntu.16.04-x64.tar.gz -O ~/quamotion-webdriver.0.1.6515-ubuntu.16.04-x64.tar.gz
+wget -nv -nc https://qmcdn.blob.core.windows.net/download/quamotion-webdriver.0.1.6515-ubuntu.16.04-x64.tar.gz -O /vagrant/quamotion-webdriver.0.1.6515-ubuntu.16.04-x64.tar.gz
 mkdir -p /usr/share/quamotion/
-tar -C /usr/share/quamotion/ -xzf ~/quamotion-webdriver.0.1.6515-ubuntu.16.04-x64.tar.gz
+tar -C /usr/share/quamotion/ -xzf /vagrant/quamotion-webdriver.0.1.6515-ubuntu.16.04-x64.tar.gz
 
 # Listen on all IP addresses; port forwarding on VMs arrives on the interface connected to the vLAN which has
 # a non-loopback IP
-cat /usr/share/quamotion/appsettings.json | jq '.webdriver.webDriverUrl="http://0.0.0.0:17894"' | tee /usr/share/quamotion/appsettings.json > /dev/null
+cat /usr/share/quamotion/appsettings.json | jq '.webdriver.webDriverUrl="http://0.0.0.0:17894"' | tee /usr/share/quamotion/appsettings.tmp.json > /dev/null
+rm /usr/share/quamotion/appsettings.json
+mv /usr/share/quamotion/appsettings.tmp.json /usr/share/quamotion/appsettings.json
 
 # Enable running as a SystemD service
 mkdir -p /usr/lib/systemd/system/
 cp /vagrant/quamotion.service /usr/lib/systemd/system/
 cp /vagrant/quamotion.sudoers /etc/sudoers.d/
+
+# If available, copy the Quamotion license file, the iOS developer disk images
+# and apps to the 
+mkdir -p /usr/share/quamotion/App_Data/
+mkdir -p /usr/share/quamotion/App_Data/apps/
+mkdir -p /usr/share/quamotion/App_Data/devimg/
+
+if [ -f /vagrant/.license ]; then
+   cp /vagrant/.license /usr/share/quamotion/App_Data/
+fi
+
+if [ -d /vagrant/devimg ]; then
+   cp -r /vagrant/devimg/ /usr/share/quamotion/App_Data/
+fi
+
+if [ -d /vagrant/apps ]; then
+   cp -r /vagrant/apps/ /usr/share/quamotion/App_Data/
+fi
+
+chown -R ubuntu /usr/share/quamotion/App_Data/
 
 # Good to go!
 systemctl start quamotion
