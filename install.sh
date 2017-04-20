437a716f6c62707b43bd7d902c1652ffcc85e537
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-04-20 02:10:11 +0200

    Use the Quamotion PPA for libimobiledevice packages

--- install.sh
+++ install.sh
@@ -1,27 +1,30 @@
 #!/bin/sh
 
+# Set a default password
+echo "ubuntu:ubuntu" | chpasswd
+
+# The version of usbmuxd which comes with Ubuntu 16.04 is slightly outdated; we publish a PPA archive with
+# udpated binaries. Alternatively, you can install from source or just use the inbox version
+# /vagrant/install-usbmuxd.sh
+add-apt-repository -y ppa:quamotion/ppa
+apt-get update
+apt-get install -y usbmuxd libimobiledevice-utils
+
 # This installs the .NET Core dependencies. Quamotion ships with .NET Core, so you don't need to install
 # .NET Core itself, but its dependencies must be installed manually.
 # This script is based on the .NET Core docker images: 
 # https://github.com/dotnet/dotnet-docker/blob/master/1.1/debian/runtime/Dockerfile
-apt-get update
 apt-get install -y libc6 libcurl3 libgcc1 libgssapi-krb5-2 libicu55 liblttng-ust0 libssl1.0.0 libstdc++6 libunwind8 libuuid1 zlib1g
-echo "ubuntu:ubuntu" | chpasswd
 
 # Install python for simple scripting
 apt-get install -y python3 python3-pip
 pip3 install selenium quamotion
 
-# usbmuxd enables connections to iOS devices, and android-sdk-platform-tools-common adds
-# udev rules for Android devices; jq is used for scripting
+# android-sdk-platform-tools-common adds udev rules for Android devices;
+# jq is used for scripting
 apt-get install -y android-sdk-platform-tools-common
 apt-get install -y libgdiplus
 apt-get install -y jq
-
-# The version of usbmuxd which comes with Ubuntu 16.04 is slightly outdated; you can install from source
-# or just use the inbox version
-# /vagrant/install-usbmuxd.sh
-apt-get install -y usbmuxd libimobiledevice-utils
 
 # Download and install the Quamotion software
 version=0.1.6525.0
