bc3585c8d802c490d29113a5f9f783470bad602c
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-04-20 15:35:23 +0200

    CentOS tryout

---
+++ centos/install.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+yum install -y libunwind gettext libcurl-devel openssl-devel zlib libicu-devel
+
+yum install -y epel-release
+yum install -y python34 python34-pip
+pip3 install selenium quamotion
+
+yum install -y android-tools
+yum install -y libimobiledevice usbmuxd
+yum install -y libgdiplus
+yum install -y jq
+
+version=0.1.6573
+wget -nv -nc https://qmcdn.blob.core.windows.net/download/mobco-centos.tar.gz -O /vagrant/quamotion-webdriver.$version-centos.7.1-x64.tar.gz
+mkdir -p /usr/local/share/quamotion/
+tar -C /usr/local/share/quamotion/ -xzf /vagrant/quamotion-webdriver.$version-ubuntu.16.04-x64.tar.gz
+
+# Listen on all IP addresses; port forwarding on VMs arrives on the interface connected to the vLAN which has
+# a non-loopback IP
+cat /usr/share/quamotion/appsettings.json | jq '.webdriver.webDriverUrl="http://0.0.0.0:17894"' | tee /usr/share/quamotion/appsettings.tmp.json > /dev/null
+cat /usr/share/quamotion/appsettings.tmp.json | jq '.webdriver.frontendEnabled=true' | tee /usr/share/quamotion/appsettings.json > /dev/null
+rm /usr/share/quamotion/appsettings.tmp.json
+
+mkdir -p /usr/lib/systemd/system/
+cp /vagrant/quamotion.service /usr/lib/systemd/system/
+
+# If available, copy the Quamotion license file, the iOS developer disk images
+# and apps to the 
+mkdir -p /usr/share/quamotion/App_Data/
+mkdir -p /usr/share/quamotion/App_Data/apps/
+mkdir -p /usr/share/quamotion/App_Data/devimg/
+
+if [ -f /vagrant/.license ]; then
+   cp /vagrant/.license /usr/share/quamotion/App_Data/
+fi
+
+if [ -d /vagrant/devimg ]; then
+   cp -r /vagrant/devimg/ /usr/share/quamotion/App_Data/
+fi
+
+if [ -d /vagrant/apps ]; then
+   cp -r /vagrant/apps/ /usr/share/quamotion/App_Data/
+fi
+
+chown -R ubuntu /usr/share/quamotion/App_Data/
+
+# Good to go!
+# Enable the quamotion service so that it persists between reboots
+systemctl enable --now quamotion
+
