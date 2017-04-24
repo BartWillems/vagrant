549fa9e01ab2406f1adf0d861b2e59f5d0e635bb
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-04-24 15:22:51 +0200

    Update installation script for CentOS to use libimobiledevice from Quamotion's repositories

--- centos/install.sh
+++ centos/install.sh
@@ -5,6 +5,9 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
+# Set a default password
+echo "vagrant:vagrant" | chpasswd
+
 yum install -y libunwind gettext libcurl-devel openssl-devel zlib libicu-devel
 
 yum install -y epel-release
@@ -12,14 +15,18 @@ yum install -y python34 python34-pip
 pip3 install selenium quamotion
 
 yum install -y android-tools
-yum install -y libimobiledevice usbmuxd
 yum install -y libgdiplus
 yum install -y jq
 
+# Download updated versions of libimobiledevice, usbmuxd
+wget -nv -nc http://download.opensuse.org/repositories/home:qmfrederik/RHEL_7/home:qmfrederik.repo -O /etc/yum.repos.d/quamotion.repo
+yum install -y libimobiledevice-devel usbmuxd
+
+# Download the Quamotion software
 version=0.1.6573
-wget -nv -nc https://qmcdn.blob.core.windows.net/download/mobco-centos.tar.gz -O /vagrant/quamotion-webdriver.$version-centos.7.1-x64.tar.gz
+wget -nv -nc https://qmcdn.blob.core.windows.net/download/quamotion-webdriver.$version-rhel.7.0-x64.tar.gz -O /vagrant/quamotion-webdriver.$version-rhel.7-x64.tar.gz
 mkdir -p /usr/local/share/quamotion/
-tar -C /usr/local/share/quamotion/ -xzf /vagrant/quamotion-webdriver.$version-centos.7.1-x64.tar.gz
+tar -C /usr/local/share/quamotion/ -xzf /vagrant/quamotion-webdriver.$version-rhel.7-x64.tar.gz
 
 # Listen on all IP addresses; port forwarding on VMs arrives on the interface connected to the vLAN which has
 # a non-loopback IP
@@ -48,9 +55,8 @@ if [ -d /vagrant/apps ]; then
    cp -r /vagrant/apps/ /usr/local/share/quamotion/App_Data/
 fi
 
-chown -R ubuntu /usr/local/share/quamotion/App_Data/
+chown -R vagrant /usr/local/share/quamotion/App_Data/
 
 # Good to go!
 # Enable the quamotion service so that it persists between reboots
 systemctl enable --now quamotion
-
